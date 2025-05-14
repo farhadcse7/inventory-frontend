3b24efa0efa22e68dd96d7e12431a373314aca70
farhadcse7 <fmfarhad23@gmail.com>
2025-05-14 23:03:49 +0600

    7. router navigation animaton

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -2,14 +2,17 @@
      <div id="layout-wrapper">
         <NavbarView />
         <div class="hori-overlay"></div>
-
-
         <div class="main-content">
             <div class="page-content">
                 <div class="container-fluid">
                     <!-- Content Here -->
-                    <router-view />
-
+                    <router-view v-slot="{ Component, route }">
+                        <transition name="route" mode="out-in">
+                            <div :key="route.name">
+                                <component :is="Component" />
+                            </div>
+                        </transition>
+                    </router-view>
                 </div>
             </div>
         </div>
@@ -18,5 +21,21 @@
 
 <script setup>
 import NavbarView from '@/components/Navbar.vue'
-
 </script>
+
+<style>
+.route-enter-from{
+    opacity: 0;
+    transform:  translateY(10px);
+}
+
+.route-enter-active,
+.route-leave-active{
+    transition: all 0.2s ease-out;
+}
+
+.route-leave-to{
+    opacity: 0;
+    transform:  translateY(10px);
+}
+</style>
