5942208effab0cb39715e32c439d196292d2a217
farhadcse7 <fmfarhad23@gmail.com>
2025-05-14 14:04:30 +0600

    5. layout setup in frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DefaultLayout from '../layouts/DefaultLayout.vue';
+import AuthLayout from '../layouts/AuthLayout.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/dashboard',
+      component: DefaultLayout,
+      meta: { requiresAuth: true },
+      children: [
+        { path: '/dashboard', name: 'dashboard', component: () => import('@/views/Dashboard.vue') }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+      path: '/auth',
+      redirect: '/login',
+      component: AuthLayout,
+      meta: { isGuest: true},
+      children: [
+        { path: '/login', name: 'login', component: () => import('@/views/Auth/Login.vue') }
+      ]
+    }
+  ]
 })
 
 export default router
