51c4ddd70eabaa497a1a4f09da2a84d40e6cf97d
farhadcse7 <fmfarhad23@gmail.com>
2025-05-16 04:09:47 +0600

    14. logout process

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,14 +3,48 @@
         <button type="button" class="btn header-item user text-start d-flex align-items-center" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <img class="rounded-circle header-profile-user" src="/src/assets/images/users/avatar-3.jpg" alt="Header Avatar">
             <span class="ms-2 d-none d-xl-inline-block user-item-desc">
-                <span class="user-name">Admin<i class="mdi mdi-chevron-down"></i></span>
+                <span class="user-name">{{ username }}<i class="mdi mdi-chevron-down"></i></span>
             </span>
         </button>
         <div class="dropdown-menu dropdown-menu-end pt-0" style="">
-            <h6 class="dropdown-header">Welcome Admin</h6>
+            <h6 class="dropdown-header">Welcome {{ username }}</h6>
             <a class="dropdown-item" href="pages-profile.html"><i class="mdi mdi-account-circle text-muted font-size-16 align-middle me-1"></i> <span class="align-middle">Profile</span></a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="auth-signout-cover.html"><i class="mdi mdi-logout text-muted font-size-16 align-middle me-1"></i> <span class="align-middle">Logout</span></a>
+            <a class="dropdown-item" href="#" @click.prevent="logout">
+                <i class="mdi mdi-logout text-muted font-size-16 align-middle me-1"></i> 
+                <span class="align-middle">Logout</span>
+            </a>
         </div>
     </div>
 </template>
+
+<script setup>
+/* All Library Import */
+import { useAuthStore } from '@/stores/auth';
+import { ref, reactive, inject } from 'vue';
+import { useRouter } from 'vue-router';
+
+/* All Instance */
+const swal = inject('$swal');
+const authStore = useAuthStore();
+const router = useRouter();
+
+/* All Variable */
+const username = authStore.getUserName || 'admin'
+
+/* Methods */
+const logout = () => {
+    authStore.removeToken();
+    authStore.removeUserName();
+    swal({
+        icon: 'success',
+        timer: 1000,
+        title: 'Successfully Logout!'
+    });
+    router.push({name: 'login'})
+}
+
+/* Hooks and Computed */
+
+
+</script>
