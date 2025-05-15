06f88f1ffcdfd9ea8a74415ae2e862b873251e94
farhadcse7 <fmfarhad23@gmail.com>
2025-05-15 15:30:05 +0600

    10. system configuration and axios client setup

---
+++ src/utils/systemaxios.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import config from '../utils/config';
+
+const baseClient = axios.create({
+    baseURL: config.inventoryAPIHost,
+    headers: {
+        "Accept": 'application/json',
+        "Content-Type": 'application/json',
+        "X-Requested-With": 'XMLHttpRequest'
+    }
+});
+
+const inventoryAxiosClient = axios.create({
+    baseURL: config.inventoryAPIHost+'/api',
+    headers: {
+        "Accept": 'application/json',
+        "Content-Type": 'application/json',
+        "X-Requested-With": 'XMLHttpRequest',
+        "Authorization": 'Bearer '+config.apiToken
+    }
+});
+
+export {
+    baseClient,
+    inventoryAxiosClient
+}
