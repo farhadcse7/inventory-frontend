be694372b24491140aabe97dd55e15a75b45b226
farhadcse7 <fmfarhad23@gmail.com>
2025-05-15 11:51:17 +0600

    login ui design

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,3 +1,81 @@
 <template>
-    Login
+ <div class="auth-bg-basic d-flex align-items-center min-vh-100">
+        <div class="bg-overlay bg-light"></div>
+        <div class="container">
+            <div class="d-flex flex-column min-vh-100 py-5 px-3">
+                <div class="row justify-content-center">
+                    <div class="col-xl-5">
+                        <div class="text-center text-muted mb-2">
+                            <div class="pb-3">
+                                <span class="logo-lg">
+                                    <img src="@/assets/images/logo-sm.svg" alt="" height="24"> <span
+                                        class="">Inventory</span>
+                                </span>
+                                <p class="text-muted font-size-15 w-75 mx-auto mt-3 mb-0">User Experience &amp; Interface
+                                    Design Strategy Saas Solution</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center my-auto">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card bg-transparent shadow-none border-0">
+                            <div class="card-body">
+                                <div class="py-3">
+                                    <div class="text-center">
+                                        <h5 class="mb-0">Welcome</h5>
+                                        <p class="text-muted mt-2">Sign in to continue.</p>
+                                    </div>
+
+                                    <form class="mt-4 pt-2">
+
+                                        <div class="form-floating form-floating-custom mb-3">
+                                            <input type="text" class="form-control" id="input-username"
+                                                placeholder="Enter User Name">
+                                            <label for="input-username">Email Address</label>
+                                            <div class="form-floating-icon">
+                                                <i class="uil uil-users-alt"></i>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="form-floating form-floating-custom mb-3 auth-pass-inputgroup">
+                                            <input type="password" class="form-control" id="password-input"
+                                                placeholder="Enter Password">
+                                            <label for="password-input">Password</label>
+                                            <div class="form-floating-icon">
+                                                <i class="uil uil-padlock"></i>
+                                            </div>
+                                        </div>
+                                    
+
+                                        <div class="form-check form-check-primary font-size-16 py-1">
+                                            <input class="form-check-input" type="checkbox" id="remember-check">
+                                            <label class="form-check-label font-size-14" for="remember-check">
+                                                Remember me
+                                            </label>
+                                        </div>
+
+                                        <div class="mt-3">
+                                            <button class="btn btn-primary w-100" type="submit">Log In</button>
+                                        </div>
+                                    </form>
+                                    
+                                    <!-- end form -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- end row -->
+            </div>
+        </div>
+        <!-- end container fluid -->
+    </div>
 </template>
+
+<style scoped>
+.logo-txt{
+    color: #F8F7FC !important;
+}
+</style>
