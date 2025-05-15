1620261f9a8f67b90d7ae0f657d0b74e2f889a3d
farhadcse7 <fmfarhad23@gmail.com>
2025-05-15 11:17:26 +0600

    8. layout setup - 2 -menu and navbar components updated

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="topnav active">
+        <nav class="navbar navbar-light navbar-expand-lg topnav-menu active">
+
+            <div class="collapse navbar-collapse active" id="topnav-menu-content">
+                <ul class="navbar-nav active">
+                    <li class="nav-item active">
+                        <a class="nav-link dropdown-toggle arrow-none active" href="index.html" id="topnav-dashboard"
+                            role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="bx bx-home-circle icon"></i>
+                            <span data-key="t-dashboard">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-toggle arrow-none active" href="index.html" id="topnav-dashboard"
+                            role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="bx bx-cart icon"></i>
+                            <span data-key="t-dashboard">POS</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-pages" role="button">
+                            <i class="bx bx-customize icon"></i>
+                            <span data-key="t-apps">Inventory</span>
+                            <div class="arrow-down"></div>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="topnav-pages">
+                            <div class="dropdown">
+                                <a class="dropdown-item dropdown-toggle arrow-none" href="#" id="topnav-projects"
+                                    role="button">
+                                    <span data-key="t-projects">Projects</span>
+                                    <div class="arrow-down"></div>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="topnav-projects">
+                                    <a href="projects-list.html" class="dropdown-item" data-key="t-p-list">Projects List</a>
+                                    <a href="projects-create.html" class="dropdown-item" data-key="t-create-new">Create
+                                        New</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-components" role="button">
+                            <i class="bx bx-layer icon"></i>
+                            <span data-key="t-components">HRM</span>
+                            <div class="arrow-down"></div>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="topnav-components">
+                            <div class="dropdown">
+                                <a class="dropdown-item dropdown-toggle arrow-none" href="#" id="topnav-map" role="button">
+                                    <span data-key="t-maps">Employee</span>
+                                    <div class="arrow-down"></div>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="topnav-map">
+                                    <a href="maps-google.html" class="dropdown-item" data-key="t-google">Employee List</a>
+                                    <a href="maps-vector.html" class="dropdown-item" data-key="t-vector">Create New</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-more" role="button">
+                            <i class="bx bx-file icon"></i>
+                            <span data-key="t-pages">CRM</span>
+                            <div class="arrow-down"></div>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="topnav-more">
+                            <div class="dropdown">
+                                <a class="dropdown-item dropdown-toggle arrow-none" href="#" id="topnav-pricing"
+                                    role="button">
+                                    <span data-key="t-pricing">Customer</span>
+                                    <div class="arrow-down"></div>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="topnav-pricing">
+                                    <a href="pricing-basic.html" class="dropdown-item" data-key="t-basic">Customer List</a>
+                                    <a href="pricing-table.html" class="dropdown-item" data-key="t-table">Create New</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
---
+++ src/components/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+     <div class="dropdown d-inline-block">
+        <button type="button" class="btn header-item user text-start d-flex align-items-center" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img class="rounded-circle header-profile-user" src="/src/assets/images/users/avatar-3.jpg" alt="Header Avatar">
+            <span class="ms-2 d-none d-xl-inline-block user-item-desc">
+                <span class="user-name">Admin<i class="mdi mdi-chevron-down"></i></span>
+            </span>
+        </button>
+        <div class="dropdown-menu dropdown-menu-end pt-0" style="">
+            <h6 class="dropdown-header">Welcome Admin</h6>
+            <a class="dropdown-item" href="pages-profile.html"><i class="mdi mdi-account-circle text-muted font-size-16 align-middle me-1"></i> <span class="align-middle">Profile</span></a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="auth-signout-cover.html"><i class="mdi mdi-logout text-muted font-size-16 align-middle me-1"></i> <span class="align-middle">Logout</span></a>
+        </div>
+    </div>
+</template>
